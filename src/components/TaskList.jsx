783b339651c77bcb1a46d9e78ea291ba2112d8d9
MintBlue06 <p1210mizu@gmail.com>
2025-05-01 16:59:26 -0300

    feat: Agregado componente TaskList.jsx [Alvarez Carlos Matias]

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+//import TaskInput from "./TaskInput";
+import TaskItem from "./TaskItem";
+
+TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+  //funcion para añadir
+  //pasar como parametro a TaskInput
+  const addTask = (text) => {
+    const newTask = { id: crypto.randomUUID(), text, completed: false };
+    setTasks((prev) => [...prev, newTask]);
+  };
+  const toggleTask = (taskId) => {
+    setTasks((prev) =>
+      prev.map((task) =>
+        task.id === taskId ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+  const deleteTask = (taskId) => {
+    setTasks((prev) => prev.filter((task) => task.id !== taskId));
+  };
+
+  return (
+    <div>
+      <ul>
+        {tasks.map((task) => (
+          <TaskItem
+            key={task.id}
+            task={task}
+            onToggleComplete={toggleTask}
+            onDelete={deleteTask}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TaskList;
