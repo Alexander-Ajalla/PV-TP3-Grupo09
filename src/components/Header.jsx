23523cd8824f0c3092f026a0c42a4dc2f6786dd7
Ivo <ivothaielvicencio@gmail.com>
2025-05-02 02:40:09 -0300

    refactor: Modificacion de el estilo de el header [Ivo Thaiel Vicencio Rosas]

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className="header" style={{color: "red", backgroundColor: "lightblue", padding: "20px", textAlign: "center"}}>
       <h1>Administrador de Tareas</h1>
       <p>Organiza y administra tus tareas fácilmente</p>
     </header>
